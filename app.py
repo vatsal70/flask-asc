bbfd560183bbe371c16cbf66e26b72cd82eafb4b
Vatsal Vohera <68066033+vatsal70@users.noreply.github.com>
2024-08-14 17:19:20 +0530

    remove ssl

--- app.py
+++ app.py
@@ -504,8 +504,8 @@ if __name__ == "__main__":
     print("Scheduler started.")
 
     try:
-        app.run(host='0.0.0.0', port=5000, debug=True, ssl_context = ("cert.pem", "key.pem"))
-        # app.run(host='0.0.0.0', port=5000, debug=True)
+        #app.run(host='0.0.0.0', port=5000, debug=True, ssl_context = ("cert.pem", "key.pem"))
+        app.run(host='0.0.0.0', port=5000, debug=True)
     except (KeyboardInterrupt, SystemExit):
         pass
     finally:
